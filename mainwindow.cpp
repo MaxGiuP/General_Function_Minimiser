802b4a65b11ffe12a5d349158e88024a1488b5fe
MaxGiuP <max.g.pla@gmail.com>
2025-01-28 13:17:55 +0000

    Created form and x4 computation

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,5 +1,6 @@
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
+#include <qmessagebox.h>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -12,3 +13,26 @@ MainWindow::~MainWindow()
 {
     delete ui;
 }
+
+void MainWindow::on_pBGRCalc_clicked()
+{
+    qDebug() << "NIg";
+    double x1, x2, x3, x4;
+    double phi = 2 - (1 + sqrt(5)) / 2;
+
+    x1 = ui->lEx1->text().toDouble();
+    x2 = ui->lEx2->text().toDouble();
+    x3 = ui->lEx3->text().toDouble();
+
+    if (x3-x2 > x2-x1)
+    {
+        x4 = phi * (x3 - x2) + x2;
+    }
+    else
+    {
+        x4 = phi * (x2 - x1) + x1;
+    }
+    ui->lEx4->setText(QString::number(x4));
+
+}
+
